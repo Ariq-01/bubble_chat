6cd4963da6283faed082781228e444621c69b05e
Ariq-01 <arc.dev.m1@gmail.com>
2026-02-14 20:36:49 +0700

    init

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'chat_page.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -7,53 +8,59 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(child: Center(
-        child:  Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          
-          children: [
-              Container(padding: const EdgeInsets.all(16),
-              child: Image.asset(
-              'assets/logo.png', height: 50,
-            cacheHeight: 100,
-            cacheWidth: 100,),
-            //const SizedBox(height: 20,),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Container(
+                padding: const EdgeInsets.all(16),
+                child: Image.asset(
+                  'assets/logo.png',
+                  height: 50,
+                  cacheHeight: 100,
+                  cacheWidth: 100,
+                ),
+                //const SizedBox(height: 20,),
               ),
 
-              const SizedBox(height: 20,),
-              Text('Welcome to Bubbles', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+              const SizedBox(height: 20),
+              Text(
+                'Welcome to Bubbles',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
 
-              const SizedBox(height: 10,),
-              // to do add 4 mdels 
+              const SizedBox(height: 10),
+              // to do add 4 mdels
               ChatButton(title: 'mode lonely', chatType: 'lonely'),
               ChatButton(title: 'Mode family', chatType: 'family'),
-              ChatButton(title: 'Mode friends', chatType: 'freinds'),
+              ChatButton(title: 'Mode friends', chatType: 'friends'),
               ChatButton(title: 'Mode bertanya', chatType: 'bertanya'),
-              
-          ],
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
 
 class ChatButton extends StatelessWidget {
   final String title;
-  final  String chatType;
+  final String chatType;
 
   ChatButton({required this.title, required this.chatType});
 
-@override 
-Widget build(BuildContext context) {
-  return ElevatedButton(
-    onPressed: () {
-      Navigator.push(context,
-      MaterialPageRoute(builder: (context) => ChatPage(chatType: chatType,)));
-    },
-    child: Text(title),
-  );
-}
-
-
-
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ChatPage(chatType: chatType)),
+        );
+      },
+      child: Text(title),
+    );
+  }
 }
