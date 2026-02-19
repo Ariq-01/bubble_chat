6dbadf69b295295d3917b5c8b3e79fc3bedd5076
Ariq-01 <arc.dev.m1@gmail.com>
2026-02-19 11:11:46 -0500

    add api key

---
+++ lib/services/itzam_service.dart
@@ -0,0 +1,4 @@
+class ItzamService {
+  static const apiKey = 'itzam_a163623a-185a-4348-99fe-89f589285840_fjghm98p3vegwep4vm36hy1lva7ywich';
+  static const _baseUrl = 'https://itz.am/v1/generate'
+}
