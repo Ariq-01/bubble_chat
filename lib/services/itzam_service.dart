e3c6ff6f3d1bc809b022ef473cc6dd270b2d66c7
Ariq-01 <arc.dev.m1@gmail.com>
2026-02-19 12:02:41 -0500

    add services for api kkey

--- lib/services/itzam_service.dart
+++ lib/services/itzam_service.dart
@@ -1,4 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
 class ItzamService {
   static const apiKey = 'itzam_a163623a-185a-4348-99fe-89f589285840_fjghm98p3vegwep4vm36hy1lva7ywich';
-  static const _baseUrl = 'https://itz.am/v1/generate'
+  static const _baseUrl = 'https://itz.am/api/v1';
+
+  static Stream<String> streamText(String input, String workflowSlug) async* {
+    final request = http.Request('POST', Uri.parse('$_baseUrl/stream/text'));
+    request.headers.addAll({
+      'Api-Key': _apiKey,
+      'ContentType': 'application/json',
+    });
+    request.body = jsonEncode({'input': input, 'workflowSlug': workflowSlug});
+
+    final response = await.http.Client().send(request);
+    await for (final chunk in response.stream.transform(utf8.decoder)) {
+      yield chunk;
+    }
+  }
+
+
 }
